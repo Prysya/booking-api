90b24a47ba96c72179987bed73c7b0ecea062bf2
Stas <bloodofabyss@yandex.ru>
2022-08-28 21:25:54 +0300

    feat: установил куки парсер, паспорт токен, момент, монгус валидатор
    feat: подключил фильтр всех ошибок в мейн
    
    feat: добавил валидацию .env файла
    
    feat: написал фильтр для перехвата всех ошибок
    
    feat: написал guard для локальной и куки аутентификации

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import * as Joi from 'joi';
+
 import { HotelsModule } from './modules/hotels/hotels.module';
 import { AuthModule } from './authentication/auth.module';
 import { UsersModule } from './modules/users/users.module';
@@ -12,14 +14,24 @@ import { HotelRoomModule } from './modules/hotel-room/hotel-room.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION_TIME: Joi.string().required(),
+        DB_HOST: Joi.string(),
+        DB_USERNAME: Joi.string(),
+        DB_PASSWORD: Joi.string(),
+        DB_NAME: Joi.string(),
+      }),
+    }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri:
           configService.get<string>('DB_HOST') ?? 'mongodb://localhost:27017',
-        // user: configService.get<string>('DB_USERNAME') ?? 'root',
-        // pass: configService.get<string>('DB_PASSWORD') ?? 'password',
+        user: configService.get<string>('DB_USERNAME') ?? 'root',
+        pass: configService.get<string>('DB_PASSWORD') ?? 'password',
         dbName: configService.get<string>('DB_NAME') ?? 'booking_database',
       }),
       inject: [ConfigService],
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './common/adapters/redisIo.adapter';
-import { RequestInterceptor } from './common/interceptors/request.interceptor';
-import { HttpExceptionFilter } from './common/exceptions/httpException.filter';
+import { AllExceptionsFilter } from './common/exceptions/all-exceptions.filter';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.useGlobalInterceptors(new RequestInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter());
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
   app.setGlobalPrefix('api');
+  app.use(cookieParser());
 
   const redisIoAdapter = new RedisIoAdapter(app);
   await redisIoAdapter.connectToRedis();
