4d496983e50c67b153ce755ad52f844bd7af9d99
Stas <bloodofabyss@yandex.ru>
2022-09-04 19:34:17 +0300

    feat(auth): написал декоратор на проверку роли

--- src/authentication/decorators/auth.decorator.ts
+++ src/authentication/decorators/auth.decorator.ts
@@ -1,11 +1,11 @@
-/*import { applyDecorators, SetMetadata, UseGuards } from "@nestjs/common";
+import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
+import { Roles } from '../../modules/users/enums/users.enum';
+import { RolesGuard } from '../guards/roles.guard';
+import JwtAuthGuard from '../../common/guards/jwt-auth.guard';
 
-export function Auth(...roles: Role[]) {
+export function Auth(roles: Roles[]) {
   return applyDecorators(
     SetMetadata('roles', roles),
-    UseGuards(AuthGuard, RolesGuard),
-    ApiBearerAuth(),
-    ApiUnauthorizedResponse({ description: 'Unauthorized' }),
+    UseGuards(JwtAuthGuard, RolesGuard),
   );
 }
-*/
